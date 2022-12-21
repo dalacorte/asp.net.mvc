affe66b10258ede34a5ff0cfae32f586cace8b4a
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2022-12-21 15:17:59 -0300

    refactoring

---
+++ MVC.NET6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC.NET6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
